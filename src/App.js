4098c92bfab908c8df695500de5c546ac58ae0d7
Marshal Marks <marshmarks@gmail.com>
2025-07-10 03:39:03 -0500

    Created homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Journal from './pages/Journal';
+import Projects from './pages/Projects';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/journal" element={<Journal />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+function Home() {
+    return (
+        <>
+            <h1>The Cyberden</h1>
+            <div id="about-section">
+                <h3>About Me</h3>
+                <p>Hi! My name is Clay Marks.</p>
+            </div>
+            <div id="links-section">
+                <p><a href="https://bsky.app/profile/cyberlizer.net?utm_source=cyberlizer.net">Bluesky</a></p>
+                <p><a href="https://github.com/MarshalMarks">GitHub</a></p>
+                <p><a href="mailto:marshmarks@gmail.com">Email</a></p>
+                <p><a href="https://talk.opn.haus/memberlist.php?mode=viewprofile&u=64&utm_source=cyberlizer.net">Open House* Forum</a></p>
+            </div>
+            <h3><a href="/journal">Journal</a></h3>
+            <h3><a href="/projects">Projects</a></h3>
+        </>
+    );
+}
+
+export default Home;
